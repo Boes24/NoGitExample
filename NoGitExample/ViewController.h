cd46de55ed8eb801d8f45ef774df722f73d628a3
Christian Boes <boes@Christians-MacBook-Pro.local>
2018-02-06 15:37:03 +0100

    Initial commit

---
+++ NoGitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoGitExample
+//
+//  Created by Christian Boes on 06/02/2018.
+//  Copyright © 2018 Christian Boes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NoGitExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NoGitExample
+//
+//  Created by Christian Boes on 06/02/2018.
+//  Copyright © 2018 Christian Boes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
